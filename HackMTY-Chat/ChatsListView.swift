12d951be6738ecb2ab13a08020437eb9ba170a1a
Santiago Quihui <santiagoquihui@hotmail.com>
2021-08-28 18:35:18 -0500

    ChatsList: Removed NavigationView and fixed chat username not udpating

--- HackMTY-Chat/ChatsListView.swift
+++ HackMTY-Chat/ChatsListView.swift
@@ -11,17 +11,36 @@ struct ChatsListView: View {
     @EnvironmentObject var firebaseManager: FirebaseManager
     @EnvironmentObject var userData: UserData
     @Binding var chats: [Chat]
+    @State private var showAddSheet = false
+    @State private var names: [String: String] = [:]
+    
     var body: some View {
-        NavigationView {
-            List(chats) { chat in
-                NavigationLink(
-                    destination: ChatView(chat: chat, currentUser: userData.username),
-                    label: {
-                        Text(firebaseManager.getOtherUserName(from: chat.users, currentUser: userData.userID))
-                    })
-            }
-            .navigationBarTitle("Chats")
+        List(chats) { chat in
+            NavigationLink(
+                destination: ChatView(chat: chat, currentUser: userData.username),
+                label: {
+                    Text(names[chat.id!] ?? "")
+                })
         }
+        .onAppear {
+            for chat in chats {
+                firebaseManager.getOtherUserName(from: chat.users, currentUser: userData.userID) { name in
+                    names[chat.id!] = name
+                }
+            }
+        }
+        .toolbar(content: {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button(action: {
+                    showAddSheet = true
+                }, label: {
+                    Image(systemName: "person.fill.badge.plus")
+                })
+            }
+        })
+        .sheet(isPresented: $showAddSheet, content: {
+            UserCodeScanner()
+        })
     }
 }
 
